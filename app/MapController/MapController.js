717ea78e8cac8af2bbc9fd9c58e52f6086d9477c
Douglas <noisyflowers@douglasmeredith.net>
2016-10-26 20:49:00 -0700

    Building legend from urls in layers objects
    Building legend from urls in layers objects

--- app/MapController/MapController.js
+++ app/MapController/MapController.js
@@ -1,6 +1,6 @@
 angular.module('MapController', [])
 
-.controller('MapController', function MapController($scope) {
+.controller('MapController', function MapController($scope, $http) {
 	
 	angular.extend($scope, {
 		center: { //Flagstaff
@@ -42,7 +42,8 @@ angular.module('MapController', [])
 					type: 'ImageWMS',
 					params: {'LAYERS': 'Wellheader', 'TRANSPARENT': 'true', 'STYLES': ' '},
 					//url: 'http://cors-anywhere.herokuapp.com/http://services.azgs.az.gov/arcgis/services/aasggeothermal/AZWellHeaders/MapServer/WMSServer'
-					url: 'http://localhost:8000/proxy/http://services.azgs.az.gov/arcgis/services/aasggeothermal/AZWellHeaders/MapServer/WMSServer'
+					url: '/proxy/http://services.azgs.az.gov/arcgis/services/aasggeothermal/AZWellHeaders/MapServer/WMSServer',
+					legend_url: '/proxy/http://services.azgs.az.gov/arcgis/services/aasggeothermal/AZWellHeaders/MapServer/WmsServer?request=GetLegendGraphic%26version=1.1.1%26format=image/png%26layer=Wellheader&legend_options=fontName:Roboto;fontAntiAliasing:true;fontColor:0x000033;fontSize:16;bgColor:0xf6f6f6;dpi:91'
 				}
 			},
 			{
@@ -52,7 +53,8 @@ angular.module('MapController', [])
 				source: {
 					type: 'TileArcGISRest',
 					params: {},
-					url: 'http://services.azgs.az.gov/arcgis/rest/services/test/Arizona_Land_Ownership/MapServer'
+					url: 'http://services.azgs.az.gov/arcgis/rest/services/test/Arizona_Land_Ownership/MapServer',
+					legend_url: 'http://services.azgs.az.gov/arcgis/rest/services/test/Arizona_Land_Ownership/MapServer/legend?f=pjson'
 				}
 			},
 			{
@@ -64,7 +66,8 @@ angular.module('MapController', [])
 					params: {'LAYERS':'AMEUP:MILITARY_INSTALLATIONS_RANGES_TRAINING_AREAS_BND'},
 					//url: 'http://10.208.3.127:8080/geoserver/AMEUP/wms'
 					//url: 'http://cors-anywhere.herokuapp.com/http://Ameup.usgin.org/geoserver/web'
-					url: 'http://localhost:8000/proxy/http://Ameup.usgin.org/geoserver/wms'
+					url: '/proxy/http://Ameup.usgin.org/geoserver/wms',
+					legend_url: '/proxy/http://ameup.usgin.org:8080/geoserver/wms?request=GetLegendGraphic&format=image/png&width=20&height=20&layer=AMEUP:MILITARY_INSTALLATIONS_RANGES_TRAINING_AREAS_BND&legend_options=fontName:Roboto;fontAntiAliasing:true;fontColor:0x000033;fontSize:16;bgColor:0xf6f6f6;dpi:91'
 				}
 			},
 			{
@@ -75,7 +78,8 @@ angular.module('MapController', [])
 					type: 'ImageWMS',
 					params: {'LAYERS':'section368:military_training_route_ir_corridor'},
 					//url: 'http://cors-anywhere.herokuapp.com/http://bogi.evs.anl.gov/geoserver/section368/wms'
-					url: 'http://localhost:8000/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms'
+					url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms',
+					legend_url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms?request=GetLegendGraphic&format=image/png&width=20&height=20&layer=section368:military_training_route_ir_corridor&legend_options=fontName:Roboto;fontAntiAliasing:true;fontColor:0x000033;fontSize:16;bgColor:0xf6f6f6;dpi:91'
 				}
 			},
 			{
@@ -86,7 +90,8 @@ angular.module('MapController', [])
 					type: 'ImageWMS',
 					params: {'LAYERS':'section368:acec2'},
 					//url: 'http://cors-anywhere.herokuapp.com/http://bogi.evs.anl.gov/geoserver/section368/wms'
-					url: 'http://localhost:8000/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms'
+					url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms',
+					legend_url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms?request=GetLegendGraphic&format=image/png&width=20&height=20&layer=section368:acec2&legend_options=fontName:Roboto;fontAntiAliasing:true;fontColor:0x000033;fontSize:16;bgColor:0xf6f6f6;dpi:91'
 				}
 			},
 			{
@@ -97,7 +102,8 @@ angular.module('MapController', [])
 					type: 'ImageWMS',
 					params: {'LAYERS':'section368:critical_habitat_esa_listed_species_area'},
 					//url: 'http://cors-anywhere.herokuapp.com/http://bogi.evs.anl.gov/geoserver/section368/wms'
-					url: 'http://localhost:8000/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms'
+					url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms',
+					legend_url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms?request=GetLegendGraphic&format=image/png&width=20&height=20&layer=section368:critical_habitat_esa_listed_species_area&legend_options=fontName:Roboto;fontAntiAliasing:true;fontColor:0x000033;fontSize:16;bgColor:0xf6f6f6;dpi:91'
 				}
 			},
 			{
@@ -108,7 +114,8 @@ angular.module('MapController', [])
 					type: 'ImageWMS',
 					params: {'LAYERS':'section368:critical_habitat_esa_listed_species_line'},
 					//url: 'http://cors-anywhere.herokuapp.com/http://bogi.evs.anl.gov/geoserver/section368/wms'
-					url: 'http://localhost:8000/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms'
+					url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms',
+					legend_url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms?request=GetLegendGraphic&format=image/png&width=20&height=20&layer=section368:critical_habitat_esa_listed_species_line&legend_options=fontName:Roboto;fontAntiAliasing:true;fontColor:0x000033;fontSize:16;bgColor:0xf6f6f6;dpi:91'
 				}
 			},
 			{
@@ -119,7 +126,8 @@ angular.module('MapController', [])
 					type: 'ImageWMS',
 					params: {'LAYERS':'section368:developable_area_sez'},
 					//url: 'http://cors-anywhere.herokuapp.com/http://bogi.evs.anl.gov/geoserver/section368/wms'
-					url: 'http://localhost:8000/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms'
+					url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms',
+					legend_url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms?request=GetLegendGraphic&format=image/png&width=20&height=20&layer=section368:developable_area_sez&legend_options=fontName:Roboto;fontAntiAliasing:true;fontColor:0x545454;fontSize:16;bgColor:0xf6f6f6;dpi:91'
 				}
 			},
 			{
@@ -130,12 +138,25 @@ angular.module('MapController', [])
 					type: 'ImageWMS',
 					params: {'LAYERS':'section368:developable_area_sez_point'},
 					//url: 'http://cors-anywhere.herokuapp.com/http://bogi.evs.anl.gov/geoserver/section368/wms'
-					url: 'http://localhost:8000/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms'
+					url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms',
+					legend_url: '/proxy/http://bogi.evs.anl.gov/geoserver/section368/wms?request=GetLegendGraphic&format=image/png&width=20&height=20&layer=section368:developable_area_sez_point&legend_options=fontName:Roboto;fontAntiAliasing:true;fontColor:0x000033;fontSize:16;bgColor:0xf6f6f6;dpi:91'
 				}
 			}
 			
 		]
 	});
 	
-
+	
+	$scope.layers.forEach(function(layer) {
+		if (layer.source.type === "TileArcGISRest") {
+			$http.get(layer.source.legend_url).
+				success(function(data, status, headers, config) {
+					layer.legend_json = data;
+				}).
+				error(function(data, status, headers, config) {
+					layer.legend_json = "not available";
+				});
+		}
+	});
+	
 });
