f7e58b7f923ec0b08027897e0e881fdc04e6e129
Douglas <noisyflowers@douglasmeredith.net>
2016-12-13 11:00:11 -0700

    Broke site into routes with ui-route

--- app/app.js
+++ app/app.js
@@ -8,11 +8,38 @@ var app = angular.module('mapApp', [
   'login',
   'ngMaterial',
   'ngResource',
-  'ngMaterialAccordion'
+  'ngMaterialAccordion',
+  'ui.router'
 ])
 .config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('blue-grey')
     .accentPalette('brown');
+})
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('cover', {
+    url: '/',
+    templateUrl: 'views/cover.html'
+  })
+  .state('map', {
+    url: '/map',
+    templateUrl: 'views/map.html',
+	controller: 'MapController'
+  });
+ 
+  $urlRouterProvider.otherwise('/');
+})
+;/*
+.run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+  $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
+    if (!AuthService.isAuthenticated()) {
+      console.log(next.name);
+      if (next.name !== 'outside.login' && next.name !== 'outside.register') {
+        event.preventDefault();
+        $state.go('outside.login');
+      }
+    }
+  });
 });
-
+*/
