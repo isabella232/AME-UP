141cc2e25892e594bdd8c85b391e201a984bb98b
Douglas <noisyflowers@douglasmeredith.net>
2017-07-13 10:49:30 -0700

    New layerInfoClicked handler

--- app/tabs/layers/layers.controller.js
+++ app/tabs/layers/layers.controller.js
@@ -10,4 +10,36 @@ angular.module('LayersTabController', ['APIService', 'SettingsService', 'MapTool
 	
 	$scope.layerSettingsData = LayersTabSettings.data;
 	$scope.layerClicked = MapTools.layerClicked;	
+	
+	$scope.layerInfoClicked = function(event, layer) {
+		console.log("layerInfoClicked");
+		console.log("layer"); console.log(layer);
+		
+		alert = $mdDialog.alert({
+			//title: layer.name,
+			locals: { layer: layer },
+			controller: DialogController,
+			templateUrl: 'tabs/layers/layer_info_dialog.html',
+			targetEvent: event,
+			ok: 'Done'
+		});
+
+		function DialogController($scope, $mdDialog, layer) {
+			console.log("dialog enter");
+			$scope.title = layer.name;
+			$scope.content = layer.source.metadata;
+			
+			$scope.closeDialog = function() {
+				$mdDialog.hide();
+			}
+		}
+		
+		$mdDialog
+			.show( alert )
+			.finally(function() {
+				alert = undefined;
+			});
+		
+		
+	}
 });
