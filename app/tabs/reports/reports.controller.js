63ba512a5fc2bafa3da6d76306fa7d45ff9faaf9
Douglas <noisyflowers@douglasmeredith.net>
2017-07-22 12:21:43 -0700

    Download pdf with pdfmake. Kind of ugly product, but it works

--- app/tabs/reports/reports.controller.js
+++ app/tabs/reports/reports.controller.js
@@ -59,7 +59,103 @@ angular.module('ReportsTabController', ['APIService', 'SettingsService', 'ngMate
 			}
 			
 			$scope.downloadClicked = function() {
-				$mdToast.show($mdToast.simple().textContent('Download not yet implemented'));			
+				//$mdToast.show($mdToast.simple().textContent('Download not yet implemented'));	
+					
+				/***
+				//Works but only adds what is visible in the scroll area.
+				html2canvas(document.getElementById('reportBody'), {
+					onrendered: function (canvas) {
+						var data = canvas.toDataURL();
+						var docDefinition = {
+							content: [{
+								image: data,
+								width: 500,
+							}]
+						};
+						pdfMake.createPdf(docDefinition).download("Report.pdf");
+					}
+				});		
+				***/
+				
+				/**
+				let tableContent = [];
+				for (contact in results.records) {
+					tableContent.push([contact.first_name, contact.last_name, contact.position_title, contact.department, contact.agency_name, contact.agency_type, contact.phone, contact.email,  contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code]);
+				}
+				**/
+
+				html2canvas(document.getElementById('introText'), {
+					onrendered: function (canvas) {
+						let data = canvas.toDataURL();
+						
+						let dateStr = new Date().toLocaleDateString();
+						
+						let docDef = {
+							pageOrientation: 'landscape',
+							footer: function(currentPage, pageCount) { return { text: currentPage.toString() + ' of ' + pageCount, alignment: 'right', margin: [20, 2] }; },
+							content: [
+								{ text: 'Contact Report ' + dateStr, fontSize: 22, bold:true },
+								{image: data, width: 740},
+								{table: {
+									headerRows: 1,
+									dontBreakRows: true,
+									body: [
+										[
+											{text: 'First Name', bold:true}, 
+											{text: 'Last Name', bold:true},
+											{text: 'Title', bold:true},
+											{text: 'Department', bold:true},
+											{text: 'Agency Name', bold:true}, 
+											{text: 'Agency Type', bold:true},
+											{text: 'Phone', bold:true}, 
+											{text: 'Email', bold:true},
+											{text: 'Address', bold:true}]
+									],
+									fontSize: 10
+								}}
+							]
+						}
+
+						//for (contact in $scope.results.records) {
+						$scope.results.records.forEach(function(contact) {
+							docDef.content[2].table.body.push([contact.first_name, contact.last_name, contact.position_title, contact.department, contact.agency_name, contact.agency_type, contact.phone, contact.email,  contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code]);
+						});
+
+						
+						pdfMake.createPdf(docDef).download("ContactReport - " + dateStr + ".pdf");
+						
+					}
+				});		
+
+				/***
+				let dateStr = new Date().toLocaleDateString();
+				
+				let docDef = {
+					pageOrientation: 'landscape',
+					content: [
+					    { text: 'Contact Report ' + dateStr, fontSize: 22, bold:true },
+						' ',
+						//$scope.results.textBoxContent[0],
+						introText,
+						' ',
+						{table: {
+							headerRows: 1,
+							body: [
+								['First Name', 'Last Name', 'Title', 'Department', 'Agency Name', 'Agency Type', 'Phone', 'Email', 'Address']
+							],
+							fontSize: 10
+						}}
+					]
+				}
+
+				//for (contact in $scope.results.records) {
+				$scope.results.records.forEach(function(contact) {
+					docDef.content[2].table.body.push([contact.first_name, contact.last_name, contact.position_title, contact.department, contact.agency_name, contact.agency_type, contact.phone, contact.email,  contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code]);
+				});
+
+				
+				pdfMake.createPdf(docDef).download("ContactReport - " +  + ".pdf");
+				***/
 			}
 			
 		}
