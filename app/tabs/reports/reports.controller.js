b13c337678500190adfb11d705a7cd500638c11f
Douglas <noisyflowers@douglasmeredith.net>
2017-09-08 14:59:26 -0700

    New logic for Military Encroachment Report

--- app/tabs/reports/reports.controller.js
+++ app/tabs/reports/reports.controller.js
@@ -1,6 +1,6 @@
 angular.module('ReportsTabController', ['APIService', 'SettingsService', 'ngMaterial', 'ngFileSaver'])
 
-.controller('ReportsTabController', function ReportsTabController($scope, $rootScope, FileSaver, Blob, Projects, MapSettings, ProjectSettings, Reports, APP_CONFIG, $mdDialog, $mdToast)
+.controller('ReportsTabController', function ReportsTabController($scope, $rootScope, FileSaver, Blob, Projects, MapSettings, ProjectSettings, Reports, APP_CONFIG, $mdDialog, $mdToast, $q)
 {
 	$scope.reportClicked = function(event, type) {
 		//console.log("reportClicked, boxExtent = " + $scope.boxExtent);
@@ -60,120 +60,219 @@ angular.module('ReportsTabController', ['APIService', 'SettingsService', 'ngMate
 			
 
 			$scope.csvDownloadClicked = function() {
-				let csvStr = 'First Name,Last Name,Title,Department,Agency Name,Agency Type,Phone,Email,Address\n';
-				$scope.results.records.forEach(function(contact) {
-					csvStr += '"' + contact.first_name + '","' + contact.last_name + '","' + contact.position_title + '","' + contact.department + '","' + contact.agency_name + '","' + contact.agency_type + '","' + contact.phone + '","' + contact.email + '","' + contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code + '"\n';
-				});
-				
-				console.log("csv = " + csvStr);
-				
-				let data = new Blob([csvStr], { type: 'text/plain;charset=utf-8' });
-				let dateStr = new Date().toLocaleDateString();
-				FileSaver.saveAs(data, "ContactReport - " + dateStr + ".csv");
+				if (reportName === "Contact") {
+					let csvStr = 'First Name,Last Name,Title,Department,Agency Name,Agency Type,Phone,Email,Address\n';
+					$scope.results.records.forEach(function(contact) {
+						csvStr += '"' + contact.first_name + '","' + contact.last_name + '","' + contact.position_title + '","' + contact.department + '","' + contact.agency_name + '","' + contact.agency_type + '","' + contact.phone + '","' + contact.email + '","' + contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code + '"\n';
+					});
+					
+					console.log("csv = " + csvStr);
+					
+					let data = new Blob([csvStr], { type: 'text/plain;charset=utf-8' });
+					let dateStr = new Date().toLocaleDateString();
+					FileSaver.saveAs(data, "ContactReport - " + dateStr + ".csv");
+				} else if (reportName === "Military Encroachment") {
+					let csvStr = 'Mil Zone ID,Restriction Type,Service,Minimum Altitude,Description,Office,Contact\n';
+					$scope.results.records.forEach(function(r) {
+						csvStr += '"' + r.military_zone_id + '","' + r.restriction_type + '","' + r.service + '","' + r.minimum_altitude + '","' + r.comment + '","' + r.office + '","' + r.contact_name + ", " +r.contact_email + ", "  + r.contact_phone + '"\n';
+					});
+					
+					console.log("csv = " + csvStr);
+					
+					let data = new Blob([csvStr], { type: 'text/plain;charset=utf-8' });
+					let dateStr = new Date().toLocaleDateString();
+					FileSaver.saveAs(data, "MilitaryEncroachmentReport - " + dateStr + ".csv");
+				}
 				
 			}
 			
 			
 			$scope.pdfDownloadClicked = function() {
-				//$mdToast.show($mdToast.simple().textContent('Download not yet implemented'));	
-					
-				/***
-				//Works but only adds what is visible in the scroll area.
-				html2canvas(document.getElementById('reportBody'), {
-					onrendered: function (canvas) {
-						var data = canvas.toDataURL();
-						var docDefinition = {
-							content: [{
-								image: data,
-								width: 500,
-							}]
-						};
-						pdfMake.createPdf(docDefinition).download("Report.pdf");
-					}
-				});		
-				***/
-				
-				/**
-				let tableContent = [];
-				for (contact in results.records) {
-					tableContent.push([contact.first_name, contact.last_name, contact.position_title, contact.department, contact.agency_name, contact.agency_type, contact.phone, contact.email,  contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code]);
-				}
-				**/
+				console.log("pdfDownloadClicked, " + reportName);
 
-				html2canvas(document.getElementById('introText'), {
-					onrendered: function (canvas) {
-						let data = canvas.toDataURL();
-						
-						let dateStr = new Date().toLocaleDateString();
-						
-						let docDef = {
-							pageOrientation: 'landscape',
-							footer: function(currentPage, pageCount) { return { text: currentPage.toString() + ' of ' + pageCount, alignment: 'right', margin: [20, 2] }; },
-							content: [
-								{ text: 'Contact Report ' + dateStr, fontSize: 22, bold:true },
-								{image: data, width: 740},
-								{table: {
-									headerRows: 1,
-									dontBreakRows: true,
-									body: [
-										[
-											{text: 'First Name', bold:true}, 
-											{text: 'Last Name', bold:true},
-											{text: 'Title', bold:true},
-											{text: 'Department', bold:true},
-											{text: 'Agency Name', bold:true}, 
-											{text: 'Agency Type', bold:true},
-											{text: 'Phone', bold:true}, 
-											{text: 'Email', bold:true},
-											{text: 'Address', bold:true}]
-									],
-									fontSize: 10
-								}}
-							]
+				if (reportName === "Contact") {
+					html2canvas(document.getElementById('introText'), {
+						onrendered: function (canvas) {
+							let data = canvas.toDataURL();
+							
+							let dateStr = new Date().toLocaleDateString();
+							
+							let docDef = {
+								pageOrientation: 'landscape',
+								footer: function(currentPage, pageCount) { return { text: currentPage.toString() + ' of ' + pageCount, alignment: 'right', margin: [20, 2] }; },
+								content: [
+									{ text: 'Contact Report ' + dateStr, fontSize: 22, bold:true },
+									{image: data, width: 740},
+									{table: {
+										headerRows: 1,
+										dontBreakRows: true,
+										body: [
+											[
+												{text: 'First Name', bold:true}, 
+												{text: 'Last Name', bold:true},
+												{text: 'Title', bold:true},
+												{text: 'Department', bold:true},
+												{text: 'Agency Name', bold:true}, 
+												{text: 'Agency Type', bold:true},
+												{text: 'Phone', bold:true}, 
+												{text: 'Email', bold:true},
+												{text: 'Address', bold:true}
+											]
+										],
+										fontSize: 10
+									}}
+								]
+							}
+
+							//for (contact in $scope.results.records) {
+							$scope.results.records.forEach(function(contact) {
+								docDef.content[2].table.body.push([contact.first_name, contact.last_name, contact.position_title, contact.department, contact.agency_name, contact.agency_type, contact.phone, contact.email,  contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code]);
+							});
+
+							
+							pdfMake.createPdf(docDef).download("ContactReport - " + dateStr + ".pdf");
+							
 						}
+					});		
+				} else if (reportName === "Military Encroachment") {
+					document.getElementById('introText').scrollIntoView();
+					html2canvas(document.getElementById('introText'), {
+						logging: true,
+						onrendered: function (introCanvas) {
+							document.getElementById('endText').scrollIntoView();
+							html2canvas(document.getElementById('endText'), {
+								logging: true,
+								onrendered: function (endCanvas) {	
+									let introData = introCanvas.toDataURL();
+									console.log("introData = "); console.log(introData);
+									console.log("introCanvas = "); console.log(introCanvas);
+									let endData = endCanvas.toDataURL();
+									console.log("endData = "); console.log(endData);
+									console.log("endCanvas = "); console.log(endCanvas);
+									
+									let dateStr = new Date().toLocaleDateString();
 
-						//for (contact in $scope.results.records) {
-						$scope.results.records.forEach(function(contact) {
-							docDef.content[2].table.body.push([contact.first_name, contact.last_name, contact.position_title, contact.department, contact.agency_name, contact.agency_type, contact.phone, contact.email,  contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code]);
-						});
+									/*******
+									//This version displays results in a simple table
+									let docDef = {
+										pageOrientation: 'landscape',
+										footer: function(currentPage, pageCount) { return { text: currentPage.toString() + ' of ' + pageCount, alignment: 'right', margin: [20, 2] }; },
+										content: [
+											{ text: 'Military Encroachment Report ' + dateStr, fontSize: 22, bold:true },
+											{image: introData, width: 740},
+											{table: {
+												headerRows: 1,
+												dontBreakRows: true,
+												body: [
+													[
+														{text: 'Mil Zone ID', bold:true}, 
+														{text: 'Restriction Type', bold:true},
+														{text: 'Service', bold:true},
+														{text: 'Minimum Altitude', bold:true},
+														{text: 'Description', bold:true}, 
+														{text: 'Office', bold:true},
+														{text: 'Contact', bold:true}
+													]
+												],
+												fontSize: 10
+											}},
+											{image: endData, width: 740}
+										]
+									}
 
-						
-						pdfMake.createPdf(docDef).download("ContactReport - " + dateStr + ".pdf");
-						
-					}
-				});		
-
-				/***
-				let dateStr = new Date().toLocaleDateString();
-				
-				let docDef = {
-					pageOrientation: 'landscape',
-					content: [
-					    { text: 'Contact Report ' + dateStr, fontSize: 22, bold:true },
-						' ',
-						//$scope.results.textBoxContent[0],
-						introText,
-						' ',
-						{table: {
-							headerRows: 1,
-							body: [
-								['First Name', 'Last Name', 'Title', 'Department', 'Agency Name', 'Agency Type', 'Phone', 'Email', 'Address']
-							],
-							fontSize: 10
-						}}
-					]
+									$scope.results.records.forEach(function(r) {
+										docDef.content[2].table.body.push([r.military_zone_id, r.restriction_type, r.service, r.minimum_altitude, r.comment, r.office, r.contact_name + ", " +r.contact_email + ", "  + r.contact_phone]);
+									});
+									********/
+									
+								
+									let docDef = {
+										pageOrientation: 'landscape',
+										footer: function(currentPage, pageCount) { return { text: currentPage.toString() + ' of ' + pageCount, alignment: 'right', margin: [20, 2] }; },
+										content: [
+											{ text: 'Military Encroachment Report ' + dateStr, fontSize: 22, bold:true },
+											{image: introData, width: 740}
+										]
+									};
+									
+									$scope.results.records.forEach(function(r) {
+										let table = {
+											table: {
+												dontBreakRows: true,
+												body: [
+													[
+														{
+															table: {
+																widths: ['*','*','*','*'],
+																headerRows: 1,
+																dontBreakRows: true,
+																body: [
+																	[
+																		{text: 'Mil Zone ID', bold:true}, 
+																		{text: 'Restriction Type', bold:true},
+																		{text: 'Service', bold:true},
+																		{text: 'Minimum Altitude', bold:true}
+																	],
+																	[
+																		r.military_zone_id, 
+																		r.restriction_type, 
+																		r.service, 
+																		r.minimum_altitude
+																	]
+																]
+															}
+														}
+													],
+													[
+														[
+															{
+																table: {
+																	headerRows: 0,
+																	dontBreakRows: true,
+																	body: [
+																		[
+																			r.comment
+																		]
+																	]
+																}
+															}
+														]
+													],
+													[
+														{
+															table: {
+																widths: ['auto','*'],
+																headerRows: 1,
+																dontBreakRows: true,
+																body: [
+																	[
+																		{text: 'Office', bold:true}, 
+																		{text: 'Contact', bold:true}
+																	],
+																	[
+																		r.office,
+																		r.contact_name,// + ", " +r.contact_email + ", "  + r.contact_phone
+																	]
+																]
+															}
+														}
+													]
+												]
+											}	
+										}
+												
+										docDef.content.push(table);
+									});
+									
+									docDef.content.push({image: endData, width: 740});
+									
+									pdfMake.createPdf(docDef).download("MilitaryEncroachmentReport - " + dateStr + ".pdf");
+								}
+							});
+						}
+					});	
 				}
-
-				//for (contact in $scope.results.records) {
-				$scope.results.records.forEach(function(contact) {
-					docDef.content[2].table.body.push([contact.first_name, contact.last_name, contact.position_title, contact.department, contact.agency_name, contact.agency_type, contact.phone, contact.email,  contact.street + ", " +contact.city + ", "  + contact.state + ", " + contact.zip_code]);
-				});
-
-				
-				pdfMake.createPdf(docDef).download("ContactReport - " +  + ".pdf");
-				***/
 			}
-			
 		}
 
 		$mdDialog
